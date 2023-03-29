d90fda690742593f40824d9b48553b21df9070ef
ferlui32 <luizferri@gmail.com>
2023-03-29 22:44:27 +0100

    added recipe info

--- frontend/src/components/RecipeCard/RecipeCard.jsx
+++ frontend/src/components/RecipeCard/RecipeCard.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
+function openWin() {
+    window.open("https://www.w3schools.com");
+  }
+
 function RecipeCard({ recipe }) {
 
-    const { label } = recipe;
+    const { label, calories, mealType, source } = recipe;
     const imageUrl = recipe.images?.SMALL?.url;
+    const nrIngredients = recipe.ingredientLines.length
 
   return recipe ? (
-    <div>
-      <h6>{label}</h6>
-      {imageUrl && <img class="shadow p-2 mb-5 bg-body-tertiary rounded" src={imageUrl} alt={label} />}
+    
+    <div class="container text-center">
+      {imageUrl && <img class="shadow p-2 mb-3 bg-body-tertiary rounded" src={imageUrl} alt={label} />}
+      <h5>{label}</h5>
+        <p>{calories.toFixed(0)} cal</p>
+        <p>{nrIngredients} ingred | {mealType}</p>
+        <p>{source}</p>
+        <form>
+            <input type="button" value="Open Recipe" onClick={openWin}/>
+        </form>
     </div>
   ) : null;
 }
