30c981630c9ccb6d161a20915f859e9c4c485dbf
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-03-29 10:13:13 +0100

    package installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import Home from "./pages/Home";
-
-import "./App.css";
+import "./fonts/Manrope/Manrope-Regular.ttf";
+import "./fonts/Montserrat/Montserrat-Italic-VariableFont_wght.ttf";
+import "./fonts/Montserrat/Montserrat-VariableFont_wght.ttf";
+import "./style/App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,18 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
-
 export default function Home() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
+      <h1>Title to test CSS H1</h1>
+      <h2>Another title H2</h2>
+      <h3>Another title h3</h3>
+      <h4>And one more</h4>
+      <p className="text">And now some text</p>
+      <ul>
+        <li>And a list</li>
+        <li>With two items</li>
+      </ul>
+      <button type="button" className="button">
+        Hello, I am button
+      </button>
     </header>
   );
 }
