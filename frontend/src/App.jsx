3bc0980137d3dbae88c62c9c53681f899c9f83a8
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-03-29 15:41:25 +0100

    add navbar page, install router packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "@pages/About-us";
+import Recipes from "@pages/Recipes";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import "./style/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipes/:nutritionalValue" element={<Recipes />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
