ff6e632480edda9ceb6ef027d96a4c9d7ef4a32b
Simão Bastos <cerejobastosa7x@gmail.com>
2023-03-31 11:24:58 +0100

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AboutUs from "@pages/About-us";
-import Recipes from "@pages/Recipes";
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import ContactUs from "./pages/Contact-us";
+import Main from "./main";
 import "./style/App.css";
-import RecipeList from '@pages/RecipeList';
-import RecipeDetails from '@components/RecipeDetails/RecipeDetails';
-import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
+    <>
     <div>
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/recipes-list" element={<RecipeList />} />
-            <Route path="/recipe/:id" element={<RecipeDetails />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-          </Routes>
-        </div>
-      </Router>
+      <Navbar />
+      <Main />
     </div>
+    </>
   );
 }
 
--- frontend/src/pages/Recipes.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Recipes() {
-  return (
-    <div>
-      <p>Recipes content</p>
-    </div>
-  );
-}
