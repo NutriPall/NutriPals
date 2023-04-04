5ab419328ecd5a5369c46cdedafc11ef2dcccc71
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-04-04 14:23:55 +0100

    Finalize menu with Routes

--- frontend/src/components/BurgerMenu/BurguerMenu.jsx
+++ frontend/src/components/BurgerMenu/BurguerMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -6,32 +7,31 @@ export default function BurgerMenu() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="navbar navbar-expand-md bg-light">
+    <div className="navbar navbar-expand-md">
       <h3 className="navbar-brand">NutriPals</h3>
       <button
         className="navbar-toggler"
         type="button"
         onClick={() => setShowMenu(!showMenu)}
-        aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className={`collapse navbar-collapse ${showMenu ? "show" : ""}`} id="navbarTogglerDemo01">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Contact
-            </a>
+          <Link className="nav-link" to="/">
+            Homepage
+          </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              About Us
-            </a>
+          <Link className="nav-link" to="/about-us">
+            About Us
+          </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Download
-            </a>
+          <Link className="nav-link" to="/recipes-list">
+            Recipe List
+          </Link>
           </li>
         </ul>
       </div>
