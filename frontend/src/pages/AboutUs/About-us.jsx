116c7791a788d1dc6ed4c56798757b9a6d748150
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-04-04 16:06:02 +0100

    Creates Our Founding Story section

---
+++ frontend/src/pages/AboutUs/About-us.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <div className="founding-story">
+      <h1>Our Founding Story</h1>
+      <ul>
+        <li>NutriPals founders came together to simplify healthy eating for everyone</li>
+        <li>With a database of thousands of recipes and the ability to sort by dietary preferences, NutriPals quickly became a go-to app for health-conscious individuals.</li>
+        <li>Today, NutriPals continues to innovate and expand its offerings to make healthy eating more accessible and enjoyable than ever before.</li>
+      </ul>
+      <img className="about-us-img" src="https://i.imgur.com/qdcdNWe.png" alt="image of a jar with colorful food" title="Cooking Healthy" />
+      </div>
+      <div className="meet-the-team">
+      </div>
+    </div>
+  );
+}
