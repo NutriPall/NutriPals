d917f529918aff286475dc9a2ec9d9eda7f6f36f
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-04-05 10:23:41 +0100

    Creates carousel with team members

--- frontend/src/components/TeamCard/TeamCard.jsx
+++ frontend/src/components/TeamCard/TeamCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import "./TeamCard.css";
 
 export default function TeamCard (props) {
     return (
-        <div className="team-card">
-            <h4>{props.fullName}</h4>
-            <img src={props.src} title={props.fullName} />
-            <p>{props.description}</p>
-        </div>
+        <Card className="team-card" style={{ width: "60%"}}>
+            <div className="title-container">            <h4>{props.fullName}</h4>
+            <a id="linkedin-img" href={props.linkedinUrl} target="_blank"><img src="https://i.imgur.com/F8Z9gM5.png" /></a>
+            </div>
+            <Card.Img className="team-member-img" src={props.src} title={props.fullName} />
+            <Card.Text>{props.description}</Card.Text>
+        </Card>
     )
 }
--- frontend/src/pages/AboutUs/About-us.jsx
+++ frontend/src/pages/AboutUs/About-us.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import TeamCard
- from "@components/TeamCard/TeamCard";
+import TeamCard from "@components/TeamCard/TeamCard";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function AboutUs() {
+  const teamMembers = [
+    {
+      fullName: "Cláudia Almeida",
+      src: "https://i.imgur.com/tsbuMRF.png",
+      linkedinUrl: "https://www.linkedin.com/in/cl%C3%A1udia-almeida-339b51185/",
+      description: "From translator to junior developer, Cláudia Almeida joined the NutriPals team to help turn the company's vision of making healthy eating easy into a reality - a task she finds much more satisfying than simply translating «eat your veggies» into multiple languages."
+    },
+    {
+      fullName: "Simão Bastos",
+      src: "https://i.imgur.com/8kvJo5W.png",
+      linkedinUrl: "https://www.linkedin.com/in/sim%C3%A3o-bastos-205a41221/",
+      description: "Introducing Simão, a language enthusiast with a diverse background and a passion for programming and real-world languages. As a part of the Nutripals team, he's working to turn our vision of easy, healthy eating into a reality."
+    },
+    {
+      fullName: "Sinde Correia",
+      src: "https://i.imgur.com/eAif7Um.png",
+      linkedinUrl: "https://www.linkedin.com/in/sinde-correia-64256a19a/",
+      description: "As an Agronomy expert, Sinde brings a wealth of knowledge and experience in agriculture to the table, helping NutriPals to develop innovative and sustainable approaches to healthy eating."
+    }
+  ];
+
   return (
     <div className="about-us-container">
       <div className="founding-story">
@@ -16,7 +37,13 @@ export default function AboutUs() {
       </div>
       <div className="meet-the-team">
         <h2>The People Making it Happen</h2>
-        <TeamCard fullName="Cláudia Almeida" src="https://i.imgur.com/tsbuMRF.png" description="From translator to junior developer, Cláudia Almeida joined the NutriPals team to help turn the company's vision of making healthy eating easy into a reality - a task she finds much more satisfying than simply translating «eat your veggies» into multiple languages." />
+        <Carousel>
+          {teamMembers.map((member, index) => (
+            <Carousel.Item key={index}>
+              <TeamCard className="team-card" fullName={member.fullName} src={member.src} linkedinUrl={member.linkedinUrl} description={member.description} />
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
