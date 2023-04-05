a2fcdcc14fa34458c92991f0bb95983cdb8234ff
LuisC92 <luis.clara@wildcodeschool.com>
2023-04-05 12:45:51 +0100

    add filters checkboxs and results

--- frontend/src/components/SearchFilter/SearchFilter.jsx
+++ frontend/src/components/SearchFilter/SearchFilter.jsx
@@ -1,26 +1,31 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
+// const arrayFilters=["Mediterranean", "Gluten-Free", "Kosher"]
 
-const SearchFilter = ({handleFilter}) => {
+const SearchFilter = ({addFilter, recipes}) => {
+  const [uniqueFilters, setUniqueFilters] = useState([])
     
+const getFilters = ()=>{
+  let tempArray = []
+  recipes.map(recipe => tempArray.push(...recipe.recipe.healthLabels))
+  let unique = [...new Set(tempArray)];
+  setUniqueFilters(unique)
+}
+
+useEffect(()=>{
+  getFilters()
+},[])
 
   return (
     <div>
-        <label htmlFor="Mediterranean"> Mediterranean
-        <input type="checkbox" name="Mediterranean"  value="Mediterranean"  onChange={handleFilter}/>
-        </label>
-        <label htmlFor="Gluten-Free"> Gluten-Free
-        <input type="checkbox" name="Gluten-Free"  value="Gluten-Free" onChange={handleFilter}/>
-        </label>
-        <label htmlFor="dairy"> Dairy
-        <input type="checkbox" name="dairy" id="dairy" value="dairy" />
-        </label>
-        <label htmlFor="shellfish"> Shellfish
-        <input type="checkbox" name="shellfish" id="shellfish" value="shellfish" />
-        </label>
-        <label htmlFor="peanuts"> Peanuts
-        <input type="checkbox" name="peanuts" id="peanuts" value="peanuts" />
-        </label>
+      {uniqueFilters.map((filter, index) =>(
+        <div key={index}>
+          <label htmlFor={filter}>{filter} </label>
+          <br/>
+          <input type="checkbox" name={filter}  value={filter}  onChange={addFilter}/>
+          <br/>
+        </div>
+      ))}
     </div>
   )
 }
