53a337d58a14ce4e3d648b4021a5905a7bba8a42
Sinde Correia <sindecorreia@gmail.com>
2023-04-06 09:25:29 +0100

    homepage

---
+++ frontend/src/components/HomePage/Home.Page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+
+export default function HomePage() {
+  return (
+    <div className="principal">
+      <div className="description">
+        <h2>
+          Cooking is one of the most beautiful aspects of human life. Through it,
+          we can experience unique flavors, aromas and textures that make us feel
+          alive and connected with the world around us. Discover a recipe with us.
+        </h2>
+      </div>
+      <div className="line1">
+        <div className="recipe1">
+          <img src="HomePageImj/recipe1.jpg" alt="recipe picture" />
+        </div>
+
+        <div className="recipe2">
+          <img src="HomePageImj/recipe2.png" alt="recipe picture" />
+        </div>
+
+        <div className="recipe3">
+          <img src="HomePageImj/recipe3.jpg" alt="recipe picture" />
+        </div>
+      </div>
+      <div className="line2">
+        <div className="recipe4">
+          <img src="HomePageImj/recipe4.jpg" alt="recipe picture" />
+        </div>
+
+        <div className="recipe5">
+          <img src="HomePageImj/recipe5.jpg" alt="recipe picture" />
+        </div>
+
+        <div className="recipe6">
+          <img src="HomePageImj/recipe6.jpg" alt="recipe picture" />
+        </div>
+      </div>
+    </div>
+  );
+}
