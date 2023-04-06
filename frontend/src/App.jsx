223a6870fb3e2ca54c2c4ad4a0a4d6519682f56a
Sinde Correia <sindecorreia@gmail.com>
2023-04-06 10:16:09 +0100

    homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AboutUs from "@pages/AboutUs/About-us";
-import Home from "./pages/Home";
 import "./style/App.css";
 import RecipeList from '@pages/RecipeList';
 import RecipeDetails from '@components/RecipeDetails/RecipeDetails';
 import ContactUs from "@pages/ContactUs/ContactUs";
 import Footer from "@components/Footer/Footer";
 import BurgerMenu from "@components/BurgerMenu/BurguerMenu";
+import HomePage from "@components/Home.Page";
+
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
           <BurgerMenu />
           <div className="routes">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/recipes-list" element={<RecipeList />} />
             <Route path="/recipe/:id" element={<RecipeDetails />} />
